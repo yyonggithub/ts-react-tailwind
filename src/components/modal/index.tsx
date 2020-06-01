6226019886b3ba21b91d46cdc581ff31e586f87b
leo <leoyyong@gmail.com>
2020-06-01 14:36:54 +0800

    feat: Improve modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,27 +1,82 @@
-import React, { FC } from "react";
+import React, { FC, MouseEventHandler } from "react";
 import ReactDOM from "react-dom";
-import './style.scss'
+import "./style.scss";
+import Button from "../button";
 
 type ModalProps = {
   toggle: boolean;
   setToggle: (toggle: boolean) => void;
+  onCancel?: MouseEventHandler;
+  onConfirm?: MouseEventHandler;
 } & Partial<typeof defaultProps>;
 
-const defaultProps = {};
+const defaultProps = {
+  hasFooter: true,
+  cancelText: "取消",
+  confirmText: "确定",
+};
 
 const Modal: FC<ModalProps> = (props) => {
-  const { toggle, setToggle, children } = props;
+  const {
+    toggle,
+    setToggle,
+    onCancel,
+    onConfirm,
+    cancelText,
+    confirmText,
+    hasFooter,
+    children,
+  } = props;
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleCancel: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setToggle(!toggle);
+    if (onCancel) {
+      onCancel(e);
+    }
+  };
+
+  const handleConfirm: MouseEventHandler<HTMLButtonElement> = (e) => {
+    if (onConfirm) {
+      onConfirm(e);
+      setToggle(!toggle);
+    }
+  };
+
+  const footer = (
+    <div className={"modal-footer"}>
+      {"onCancel" in props ? (
+        <Button
+          className={"modal-confirm"}
+          preset={"primary"}
+          onClick={handleConfirm}
+        >
+          {confirmText}
+        </Button>
+      ) : null}
+      {"onConfirm" in props ? (
+        <Button
+          className={"modal-cancel"}
+          preset={"danger"}
+          onClick={handleCancel}
+        >
+          {cancelText}
+        </Button>
+      ) : null}
+    </div>
+  );
   return toggle
     ? ReactDOM.createPortal(
         <div className={"modal-container"}>
           <div className={"modal-inner"}>
-            <button
-              className={"modal-close"}
-              onClick={() => {
-                setToggle(!toggle);
-              }}
-            />
+            <button className={"modal-close"} onClick={handleToggle} />
             {children}
+            {hasFooter && ("onCancel" in props || "onConfirm" in props)
+              ? footer
+              : null}
           </div>
         </div>,
         document.querySelector("body") as Element
--- src/modules/modal/index.tsx
+++ src/modules/modal/index.tsx
@@ -6,13 +6,20 @@ import ModalBody from "../../components/modal/modal-body";
 
 const ModalModule: FC = () => {
   const [toggle, setToggle] = useState(false);
+  const cancel = ()=>{
+    console.log('cancel');
+
+  }
+  const confirm = ()=>{
+    console.log('confirm')
+  }
   return (
     <>
       <DocGroup name="default">
         <button type="button" onClick={() => setToggle(!toggle)}>
           切换
         </button>
-        <Modal toggle={toggle} setToggle={setToggle}>
+        <Modal toggle={toggle} setToggle={setToggle} onCancel={cancel} onConfirm={confirm}>
           <ModalHeader>I am a header</ModalHeader>
           <ModalBody>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, ducimus eius tempora corrupti blanditiis quos perspiciatis sit sint placeat non velit ullam libero nemo tenetur consequatur illo ab id cupiditate.</ModalBody>
         </Modal>
