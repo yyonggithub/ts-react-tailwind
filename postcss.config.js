110d5a8ba46dfe2370d43f9f900dcdcc59e0676f
leo <leoyyong@gmail.com>
2020-06-19 18:17:30 +0800

    feat: update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,22 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: [
     "./src/**/*.html",
-    "./src/**/*.vue",
     "./src/**/*.jsx",
     "./src/**/*.js",
     "./src/**/*.ts",
     "./src/**/*.tsx"
   ],
 
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  // This is the function used to extract class names from your templates
+  defaultExtractor: content => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  }
 });
 
 module.exports = {
